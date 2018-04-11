a99d876872ba4bfb86e0d58e83747c8eed69d631
Clint <1054484+cjshaw@users.noreply.github.com>
2018-04-11 12:55:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/shawasssisignment1/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.shawasssisignment1;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView myName;
+    private TextView touchWin;
+    private ImageButton winchesterCharlie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.myName = findViewById(R.id.myNameText);
+
+        this.touchWin = findViewById(R.id.touchCharlie);
+
+        final ImageButton button = findViewById(R.id.winCharlie);
+
+        //creates audio object
+        final MediaPlayer bang = MediaPlayer.create(this, R.raw.bang);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                bang.start(); //plays sound
+            }
+        });
+    }
+}
