5f53be882bf2d3771799abfcb97a3bf2aa4ba85e
Clint <1054484+cjshaw@users.noreply.github.com>
2018-04-15 22:33:48 -0700

    Assignment 2 int commit

--- app/src/main/java/com/example/shawasssisignment1/MainActivity.java
+++ app/src/main/java/com/example/shawasssisignment1/MainActivity.java
@@ -5,13 +5,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.icu.util.Calendar;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -27,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView username;
     private EditText editUsername;
     private TextView birthdayDate;
-    private EditText birthdayEdit;
     private DatePickerDialog.OnDateSetListener dateSetListener;
+    private String date;
+    private TextView calendar;
 
 
     @Override
@@ -44,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         editEmail = findViewById(R.id.emailEdit);
         submitBtn = findViewById(R.id.submitBtn);
-        birthdayDate = findViewById(R.id.birthday);
-        birthdayEdit = findViewById(R.id.birthdayEdit);
-        
+        calendar = findViewById(R.id.birthday);
+
+
     }
 
 
@@ -62,6 +63,14 @@ public class MainActivity extends AppCompatActivity {
                 dateSetListener, year, month, day);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         dialog.show();
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                date = month + "/" + dayOfMonth + "/" + year;
+                birthdayDate.setText(date);
+            }
+        };
     }
 
 
