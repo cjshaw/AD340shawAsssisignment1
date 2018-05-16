37fcac3723ad644a618a1f76229eaaa353fce27a
Clint <1054484+cjshaw@users.noreply.github.com>
2018-05-16 15:33:44 -0700

    added more tests

--- app/src/main/java/com/example/shawasssisignment1/SecondActivity.java
+++ app/src/main/java/com/example/shawasssisignment1/SecondActivity.java
@@ -58,6 +58,7 @@ public class SecondActivity extends AppCompatActivity implements MatchesTabFragm
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+        finish();
     }
 
     @Override
@@ -70,9 +71,7 @@ public class SecondActivity extends AppCompatActivity implements MatchesTabFragm
 
     @Override
     protected void onPause() {
-        if (viewModel != null) {
-            viewModel.clear();
-        }
+        viewModel.clear();
         super.onPause();
     }
 }
