f671c55cc48fb5988afa637b550b271391b32784
Clint <1054484+cjshaw@users.noreply.github.com>
2018-05-16 18:08:05 -0700

    added tests to android test folder

--- app/src/androidTest/java/com/example/shawasssisignment1/SecondActivityTest.java
+++ app/src/androidTest/java/com/example/shawasssisignment1/SecondActivityTest.java
@@ -21,6 +21,7 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.example.shawasssisignment1.TestUtils.withRecyclerView;
 import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
 
 public class SecondActivityTest {
 
@@ -199,6 +200,23 @@ public class SecondActivityTest {
                 .atPositionOnView(4, R.id.card_title))
                 .check(matches(withText("Money man Ben")));
 
+    }
+
+    @Test
+    public void testMyConstants() {
+        assertEquals("name", MyConstants.KEY_NAME);
+        assertEquals("bday_btn_txt", MyConstants.KEY_BDAY_BTN_TXT);
+        assertEquals("profile_img", MyConstants.KEY_IMG);
+        assertEquals("user_age", MyConstants.KEY_AGE);
+        assertEquals("user_description", MyConstants.KEY_DESC);
+        assertEquals("user_occupation", MyConstants.KEY_OCC);
+        assertEquals("recycleview_state", MyConstants.KEY_RV_STATE);
+        assertEquals("You gotta have a job to get a date.", MyConstants.OCC_MSG);
+        assertEquals("You must 18 or older, kiddo.", MyConstants.DOB_MSG);
+        assertEquals("Re-enter a valid email!", MyConstants.EMAIL_MSG);
+        assertEquals("Please enter a name!", MyConstants.NAME_MSG);
+        assertEquals("Your age is: ", MyConstants.AGE_MSG);
+        assertEquals("Please select a photo", MyConstants.IMG_MSG);
     }
 
     @Test
